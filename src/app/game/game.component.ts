b14416ff8422648257aa9fc3ca8569fe656d15d2
Steven <bracke.s85@gmail.com>
2025-08-11 21:58:49 +0200

    installed angular material

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf, NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgStyle, NgIf, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game?: Game;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      const card = this.game!.stack.pop();
+      if (card !== undefined) {
+        this.currentCard = card;
+        this.pickCardAnimation = true;
+        console.log('new card: ' + card);
+        console.log(this.game);
+        
+        setTimeout(() => {
+          this.game!.playedCars .push(card);
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game)
+  }
 }
